780c246add5a23f80212ffcd6d5619ae91918c97
Júllya Cabral <124018755+Vct-Jully@users.noreply.github.com>
2023-12-18 22:05:26 -0300

    Create array-bidimensional.c

---
+++ array-bidimensional.c
@@ -0,0 +1,14 @@
+//matriz é um array com duas dimensôes
+
+int veto[19]; //vetor normal
+
+int matrizz1[3][3] = {1,2,3,4,5,6,7,8,9}
+/*visual: | 1  2  3 |
+          | 4  5  6 |
+          | 7  8  9 | */
+
+int matrizz1[3][0] = {1,2,3,4,5,6,7}
+/*visual: | 1  2  3 |
+          | 4  5  6 |
+          | 7  0  0 | */
+  
